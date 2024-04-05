d3a63dd7767100784edc556c8cf642329258e0cf
LBielskus <liudbiel@gmail.com>
2024-04-05 19:26:10 +0300

    commit

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -6,33 +6,41 @@ export default async function handle(req, res) {
 
   await mongooseConnect();
 
-  if (method === 'POST') {
-    const { name, parentCategory } = req.body;
+  try {
+    if (method === 'POST') {
+      const { name, parentCategory, imageUrl } = req.body;
 
-    const categoryDoc = await Category.create({
-      name,
-      parent: parentCategory || undefined,
-    });
-    res.json(categoryDoc);
-  }
+      const categoryDoc = await Category.create({
+        name,
+        parent: parentCategory || undefined,
+        imageUrl,
+      });
+      res.json(categoryDoc);
+    }
 
-  if (method === 'GET') {
-    res.json(await Category.find().populate('parent'));
-  }
+    if (method === 'GET') {
+      const categories = await Category.find().populate('parent');
+      res.json(categories);
+    }
 
-  if (method === 'PUT') {
-    const { name, parentCategory, _id } = req.body;
+    if (method === 'PUT') {
+      const { name, parentCategory, _id } = req.body;
 
-    const categoryDoc = await Category.updateOne(
-      { _id },
-      { name, parent: parentCategory || undefined }
-    );
-    res.json(categoryDoc);
-  }
+      const categoryDoc = await Category.findOneAndUpdate(
+        { _id },
+        { name, parent: parentCategory || undefined },
+        { new: true }
+      );
+      res.json(categoryDoc);
+    }
 
-  if (method === 'DELETE') {
-    const { _id } = req.query;
-    await Category.deleteOne({ _id });
-    res.json('ok');
+    if (method === 'DELETE') {
+      const { _id } = req.query;
+      await Category.deleteOne({ _id });
+      res.json({ message: 'Category deleted successfully' });
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 }
