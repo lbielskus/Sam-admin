bf6f82bc59b6e6faf2cf49c5c7f81ec61aa9d477
LBielskus <liudbiel@gmail.com>
2024-04-05 21:06:44 +0300

    commit

--- pages/categories.js
+++ pages/categories.js
@@ -46,7 +46,7 @@ export default function Categories() {
       toast.success('Category created successfully');
     }
     setName('');
-    setImages(''); // Reset the Images state after saving
+    setImages('');
     setParentCategory('');
     fetchCategories();
   }
@@ -65,17 +65,35 @@ export default function Categories() {
     toast.success('Category deleted!!');
   }
 
-  async function uploadImage(file) {
-    try {
-      const formData = new FormData();
-      formData.append('file', file);
-      const response = await axios.post('/api/upload', formData);
-      const uploadedImages = response.data.url;
-      setImages(uploadedImages); // Update the state with the uploaded image URL
-      toast.success('Image uploaded successfully');
-    } catch (error) {
-      console.error('Error uploading image:', error);
-      toast.error('Failed to upload image');
+  async function uploadImage(ev) {
+    const files = ev.target?.files;
+    if (files?.length > 0) {
+      setIsUploading(true);
+      const uploadImagesQueue = [];
+
+      for (const file of files) {
+        const formData = new FormData();
+        formData.append('file', file);
+
+        uploadImagesQueue.push(
+          axios.post('/api/upload', formData).then((res) => {
+            return res.data.url;
+          })
+        );
+      }
+
+      try {
+        const uploadedImageUrls = await Promise.all(uploadImagesQueue);
+        setImages(uploadedImageUrls);
+        setIsUploading(false);
+        toast.success('Images uploaded');
+      } catch (error) {
+        console.error('Error uploading images:', error);
+        setIsUploading(false);
+        toast.error('Failed to upload images');
+      }
+    } else {
+      toast.error('No files selected!');
     }
   }
 
@@ -148,7 +166,6 @@ export default function Categories() {
                   </select>
                 </div>
                 <div>
-                  {/* Input field for uploading image */}
                   <input
                     type='file'
                     accept='image/*'
@@ -192,92 +209,29 @@ export default function Categories() {
                 </tr>
               </thead>
               <tbody className='bg-white divide-y divide-gray-200'>
-                {categories.map((category, index) => (
-                  <tr
-                    key={category._id}
-                    className={index % 2 === 0 ? 'bg-gray-50' : 'bg-white'}
-                  >
-                    <td className='whitespace-nowrap px-2 py-2 sm:px-4 sm:py-2 font-medium text-gray-700'>
-                      {index + 1}
-                    </td>
-                    <td className='whitespace-nowrap px-2 py-2 sm:px-4 sm:py-2 text-gray-700'>
+                {categories.map((category, idx) => (
+                  <tr key={category._id}>
+                    <td className='px-2 py-2 sm:px-4 sm:py-3'>{idx + 1}</td>
+                    <td className='px-2 py-2 sm:px-4 sm:py-3'>
                       {category.name}
                     </td>
-                    <td className='whitespace-nowrap px-2 py-2 sm:px-4 sm:py-2 text-gray-700'>
-                      {category?.parent?.name}
+                    <td className='px-2 py-2 sm:px-4 sm:py-3'>
+                      {category.parent ? category.parent.name : 'No parent'}
                     </td>
-                    <td className='whitespace-nowrap px-2 py-2 sm:px-4 sm:py-2'>
-                      <div className='flex flex-col sm:flex-row gap-2'>
-                        <Link
+                    <td className='px-2 py-2 sm:px-4 sm:py-3'>
+                      <div className='flex space-x-1'>
+                        <button
                           onClick={() => editCategory(category)}
-                          href={'/categories'}
-                          className='inline-block rounded bg-green-600 px-2 py-1 text-xs sm:text-sm sm:px-4 sm:py-2 font-medium text-white hover:bg-green-700 text-center'
+                          className='bg-blue-100 text-blue-700 px-2 py-1 rounded-md hover:bg-blue-200 hover:text-blue-800 focus:outline-none focus:bg-blue-200 focus:text-blue-800'
                         >
                           Edit
-                        </Link>
-                        <div
-                          onKeyDown={() => setShowModal(false)}
-                          className='inline-block rounded bg-red-600 text-xs sm:text-sm font-medium text-white hover:bg-red-700 text-center'
+                        </button>
+                        <button
+                          onClick={() => deleteCategory(category)}
+                          className='bg-red-100 text-red-700 px-2 py-1 rounded-md hover:bg-red-200 hover:text-red-800 focus:outline-none focus:bg-red-200 focus:text-red-800'
                         >
-                          <button
-                            onClick={toggleModal}
-                            className='px-2 py-1 sm:px-4 sm:py-2'
-                          >
-                            Delete
-                          </button>
-                          {showModal && (
-                            <>
-                              <div className='fixed inset-0 z-10 bg-gray-300/50'></div>
-                              <div className='fixed inset-0 z-50 flex items-center justify-center p-4 sm:p-0'>
-                                <div className='mx-auto w-full overflow-hidden rounded-lg bg-white shadow-xl sm:max-w-sm'>
-                                  <div className='relative p-5'>
-                                    <div className='text-center'>
-                                      <div className='mx-auto mb-5 flex h-10 w-10 items-center justify-center rounded-full bg-red-100 text-red-500'>
-                                        <svg
-                                          xmlns='http://www.w3.org/2000/svg'
-                                          fill='none'
-                                          viewBox='0 0 24 24'
-                                          strokeWidth='1.5'
-                                          stroke='currentColor'
-                                          className='h-6 w-6'
-                                        >
-                                          <path
-                                            strokeLinecap='round'
-                                            strokeLinejoin='round'
-                                            d='M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0'
-                                          />
-                                        </svg>
-                                      </div>
-                                      <div>
-                                        <h3 className='text-lg font-medium text-gray-700'>
-                                          Delete blog post
-                                        </h3>
-                                        <div className='mt-2 text-sm text-gray-500 max-w-sm'>
-                                          Are you sure you want to delete this{' '}
-                                          {category?.name}?
-                                        </div>
-                                      </div>
-                                    </div>
-                                    <div className='mt-5 flex justify-end gap-3'>
-                                      <button
-                                        onClick={closeModal}
-                                        className='flex-1 rounded-lg border border-gray-300 bg-white px-2 py-1 sm:px-4 sm:py-2 text-center text-xs sm:text-sm font-medium text-gray-700 shadow-sm transition-all hover:bg-gray-100 focus:ring focus:ring-gray-100 disabled:cursor-not-allowed disabled:border-gray-100 disabled:bg-gray-50 disabled:text-gray-400'
-                                      >
-                                        Cancel
-                                      </button>
-                                      <button
-                                        className='flex-1 rounded-lg border border-red-500 bg-red-500 px-2 py-1 sm:px-4 sm:py-2 text-center text-xs sm:text-sm font-medium text-white shadow-sm transition-all hover:border-red-700 hover:bg-red-700 focus:ring focus:ring-red-200 disabled:cursor-not-allowed disabled:border-red-300 disabled:bg-red-300'
-                                        onClick={() => deleteCategory(category)}
-                                      >
-                                        Delete
-                                      </button>
-                                    </div>
-                                  </div>
-                                </div>
-                              </div>
-                            </>
-                          )}
-                        </div>
+                          Delete
+                        </button>
                       </div>
                     </td>
                   </tr>
